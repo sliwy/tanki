4c5de384aa625b9a96d6818560aca208a517220f
sliwy <macieksliwowski@gmail.com>
2017-02-02 16:56:44 +0100

    license added

--- tanki.py
+++ tanki.py
@@ -1,15 +1,43 @@
 #!/usr/bin/env/python
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov 13 18:22:54 2016
+The MIT License (MIT)
 
-@author: maciej
+Copyright (c) 2016 Maciej Śliwowski
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Graphical elements designed by Paulina Jastrzębska.
+Sound effects downloaded from links presented below on the Creative Commons License (Attribution 3.0 unported https://creativecommons.org/licenses/by/3.0/).
+https://www.freesound.org/people/ProjectsU012/sounds/333785/
+https://www.freesound.org/people/jivatma07/sounds/173858/
+https://www.freesound.org/people/AlaskaRobotics/sounds/221560/
+https://www.freesound.org/people/jeremysykes/sounds/341238/
+https://www.freesound.org/people/funhouse/sounds/2513/
+https://www.freesound.org/people/smcameron/sounds/51464/
+https://www.freesound.org/people/mialena24/sounds/364343/ 
 """
+
 import pygame
 
 from pygame.locals import *
 import time
-import copy
 import pickle
 import numpy as np
 from MapElement import *
